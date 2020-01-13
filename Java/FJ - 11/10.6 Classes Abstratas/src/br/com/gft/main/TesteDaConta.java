e00f03ecbf2e0b9e2dfb8ace6e6f897f0da7314b
Oosasukel <rodrigo_gonn@hotmail.com>
2020-01-13 11:39:52 -0300

    Atualizacao

--- Java/FJ - 11/10.6 Classes Abstratas/src/br/com/gft/main/TesteDaConta.java
+++ Java/FJ - 11/10.6 Classes Abstratas/src/br/com/gft/main/TesteDaConta.java
@@ -1,14 +1,23 @@
 package br.com.gft.main;
 
 import br.com.gft.model.Conta;
+import br.com.gft.model.ContaCorrente;
+import br.com.gft.model.ContaPoupanca;
 
 public class TesteDaConta {
 	public static void main(String[] args) {
-		Conta c = new Conta();
+		Conta c = new ContaCorrente();
 		
 		c.setTitular("Rodrigo");
 		
 		if(c.deposita(50)) {
+			System.out.println("Sucesso.");
+		}
+		else {
+			System.out.println("Falha.");
+		}
+		
+		if(c.saca(30)) {
 			System.out.println("Sucesso.");
 		}
 		else {
---
+++ Java/FJ - 11/10.6 Classes Abstratas/src/br/com/gft/model/ContaCorrente.java
@@ -0,0 +1,8 @@
+package br.com.gft.model;
+
+public class ContaCorrente extends Conta {
+	@Override
+	public String getTipo() {
+		return "Conta-Corrente";
+	}
+}
