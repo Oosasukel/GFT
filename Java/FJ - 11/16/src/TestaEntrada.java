a03ded94f761db06d3dc71dc06fc1eae08b9fc50
Oosasukel <rodrigo_gonn@hotmail.com>
2020-01-14 14:57:51 -0300

    Atualizacao

---
+++ Java/FJ - 11/16/src/TestaEntrada.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class TestaEntrada {
+	public static void main(String[] args) throws IOException {
+        InputStream is = new FileInputStream("arquivo.txt");
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        
+        String s = br.readLine();
+        
+        System.out.println(s);
+    }
+}
